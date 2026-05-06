2d09cb4877060d11a74fb901db9caf72333c7454
koffiyaomondesir <koffiyaomondesir324@gmail.com>
2026-05-06 00:15:17 +0000

    Reorder query fields for readability

--- database_logic.py
+++ database_logic.py
@@ -10,7 +10,7 @@ def update_account_db(account_id, name, email, balance):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "UPDATE accounts SET email = %s, balance = %s WHERE id = %s",
-                (email, balance or 0, account_id)
+                "UPDATE accounts SET balance = %s, name = %s, email = %s WHERE id = %s",
+                (balance or 0, name, email, account_id)
             )
             conn.commit()
